b85160d4c2ee2a420a4e88a5ce329dd80a6bc993
joaon <joaon@DESKTOP-MC63628>
2016-10-27 22:22:47 -0200

    Criação com método Conn na classe Conexao da camada Persistencia

--- Persistencia/Conexao.cs
+++ Persistencia/Conexao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace Persistencia
 {
     class Conexao
     {
+        public string Conn()
+        {
+            OracleCommand oComando = new OracleCommand();
+            oComando.Connection = new OracleConnection("Data Source=localhost:1521:xe;Persist Security Info=True;User ID=system;Password=123456");
+            oComando.CommandText = "select sysdate from dual";
+            oComando.CommandType = System.Data.CommandType.Text;
+            oComando.Connection.Open();
+            return oComando.ExecuteScalar().ToString();
+        }
     }
 }
